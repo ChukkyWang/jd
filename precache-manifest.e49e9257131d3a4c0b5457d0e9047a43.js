5480eef840446458ee7bbd5f6fe0a02014005730
ChukkyWang <ezechukwukae@gmail.com>
2020-07-27 11:47:02 +0100

    Updates

---
+++ precache-manifest.e49e9257131d3a4c0b5457d0e9047a43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe455918de6bcbfd08e12080210768f",
+    "url": "/jd/index.html"
+  },
+  {
+    "revision": "a357ece507cd18e2522c",
+    "url": "/jd/static/css/main.13aa8549.chunk.css"
+  },
+  {
+    "revision": "e7a90ab270e6425ffdf9",
+    "url": "/jd/static/js/2.577b24dc.chunk.js"
+  },
+  {
+    "revision": "d39df4a9385d93d435839bd506dd69f0",
+    "url": "/jd/static/js/2.577b24dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a357ece507cd18e2522c",
+    "url": "/jd/static/js/main.8193c646.chunk.js"
+  },
+  {
+    "revision": "e63797de9211eca3c16b",
+    "url": "/jd/static/js/runtime-main.12a452b3.js"
+  },
+  {
+    "revision": "d3e603ff1ff43e5c59605402e15b89f9",
+    "url": "/jd/static/media/welcome.d3e603ff.jpg"
+  }
+]);
